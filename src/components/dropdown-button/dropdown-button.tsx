7a6cb86405b6cac01bbc118270cd7b16e08c0901
Cory <cbosselmann1029@gmail.com>
2023-03-08 18:20:13 -0600

    getting basic structure

--- src/components/dropdown-button/dropdown-button.tsx
+++ src/components/dropdown-button/dropdown-button.tsx
@@ -11,7 +11,7 @@ export class DropdownButton {
   options: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'];
 
   renderMenuItems() {
-    this.options.map((option) => {
+    return this.options.map((option) => {
       return <li>{option}</li>;
     });
   }
@@ -19,11 +19,22 @@ export class DropdownButton {
   render() {
     return (
       <Host>
-        <button>Select an Option</button>
-        <ol class="menu">
-          {this.renderMenuItems()}
-        </ol>
-        <slot></slot>
+        <div class="dropdown-container">
+          <button class='select'>Select an Option</button>
+          <ol class="menu" aria-expanded="false">
+            {/* {this.renderMenuItems()} */}
+            <li><a href="#">option 1</a></li>
+            <li><a href="#">option 2</a></li>
+            <li><a href="#">option 3</a>
+              <ol class="sub-menu">
+                <li><a href="#">option 1</a></li>
+                <li><a href="#">option 2</a></li>
+                <li><a href="#">option 3</a></li>
+              </ol>
+            </li>
+            <li><a href="#">option 4</a></li>
+          </ol>
+        </div>
       </Host>
     );
   }
